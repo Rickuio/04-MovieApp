bda11c897eb945a059608c808608dba521bfa3bf
Ricardo Anchaluisa <ricardoanchaluisa@gmail.com>
2024-11-04 14:42:44 -0500

    hidden api key

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -3,7 +3,7 @@ import { AxiosAdapter } from "./http/axios.adapter";
 export const movieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: '8dfe19cfd1bdbed93488ef1d2ffde4fb',
+        api_key: 'here_your_api_key',
         language: 'es',
     }
 });
